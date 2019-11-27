109dd7f78479af4e8bd54082cf7f251136babdaf
faridzweb <farid.berrabah@gmail.com>
2019-11-27 10:39:48 +0100

    add class for exercice 1

---
+++ city.py
@@ -0,0 +1,7 @@
+class City:
+    def __init__(self, ville, dep):
+        self.ville = ville
+        self.dep = dep
+
+    def show_location(self):
+        print("la ville {} est dans le département {}".format(self.ville,self.dep))
---
+++ main.py
@@ -0,0 +1,7 @@
+from city import City
+
+paris = City("Paris", 75)
+lille = City("Lille", 59)
+lyon = City("Lyon", 69)
+marseille = City("Marseille", 13)
+paris.show_location()
